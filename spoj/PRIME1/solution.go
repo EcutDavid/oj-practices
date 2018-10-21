3a094ce78bfbf5a40b9ea1795895aff3f613ca7f
David Guan <davidguandev@gmail.com>
2018-10-21 21:27:25 +1100

    one more problem

---
+++ spoj/PRIME1/solution.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func isPrime(num int) bool {
+	if num == 1 {
+		return false
+	}
+	if num == 2 {
+		return true
+	}
+	// A slight faster......
+	if num%2 == 0 {
+		return false
+	}
+	ceil := int(math.Sqrt(float64(num)))
+	for i := 3; i <= ceil; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	t, _ := strconv.Atoi(readLine())
+	for i := 0; i < t; i++ {
+		if i > 0 {
+			fmt.Println()
+		}
+		l, r := 0, 0
+		fmt.Sscanf(readLine(), "%d %d", &l, &r)
+		for j := l; j <= r; j++ {
+			if isPrime(j) {
+				fmt.Println(j)
+			}
+		}
+	}
+}
+
+var scanner = bufio.NewScanner(os.Stdin)
+
+func readLine() string {
+	scanner.Scan()
+	return scanner.Text()
+}
