951461a238042d9778de3abf3f71ba031c533861
David Guan <davidguandev@gmail.com>
2018-10-29 21:21:53 +1100

    automated commit from ./publish.go

--- codeforces/1068/B/solution.go
+++ codeforces/1068/B/solution.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	var b, res int64
+	var b, res int64 = 0, 1
 	fmt.Scan(&b)
-	for i := int64(1); i <= b; i++ {
-		if 1e10%i == 0 {
+	if b > 1 {
+		// Count 1 and b itself.
+		res++
+		// Why Ceil instead of Floor? Example: 12
+		sqrtB := int64(math.Ceil(math.Sqrt(float64(b))))
+		if sqrtB*sqrtB == b {
 			res++
+		}
+
+		for i := int64(2); i < sqrtB; i++ {
+			if b%i == 0 {
+				res += 2
+			}
 		}
 	}
 	fmt.Println(res)
