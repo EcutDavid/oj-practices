15737d9e992cbe4db6958a5aa93a44cd3ffd58d7
David Guan <davidguandev@gmail.com>
2019-01-09 09:51:26 +1100

    some dp fun before work

---
+++ atcoder/dp/dp_f/solution.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	s, t := "", ""
+	fmt.Scan(&s, &t)
+
+	memo := make([][]int, len(s)+1)
+	for i := 0; i <= len(s); i++ {
+		memo[i] = make([]int, len(t)+1)
+	}
+	for i := 1; i <= len(s); i++ {
+		for j := 1; j <= len(t); j++ {
+			if s[i-1] == t[j-1] {
+				memo[i][j] = memo[i-1][j-1] + 1
+				continue
+			}
+			memo[i][j] = max(memo[i-1][j], memo[i][j-1])
+		}
+	}
+
+	bestLen := memo[len(s)][len(t)]
+	// Go backward to construct the solution
+	solution := ""
+	for i, j := len(s)-1, len(t)-1; len(solution) != bestLen; {
+
+		if s[i] == t[j] {
+			solution, i, j = s[i:i+1]+solution, i-1, j-1
+			continue
+		}
+		if memo[i][j+1] > memo[i+1][j] {
+			i--
+		} else {
+			j--
+		}
+	}
+	fmt.Println(solution)
+}
