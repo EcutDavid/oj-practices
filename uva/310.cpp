381199aa94a1a2aeaf96ee4babf9ca73bb9094e6
David Guan <davidguandev@gmail.com>
2019-01-15 21:17:13 +1100

    uva 310

---
+++ uva/310.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  unsigned int b;
+  while (cin >> b) {
+    unsigned int p, m;
+    cin >> p >> m;
+    b = b % m;
+
+    int res = 1;
+    set<int> resSet = {res};
+    map<int, int> indexRes, resIndex;
+    indexRes[0] = res, resIndex[res] = 0;
+
+    for (unsigned int i = 1; i <= p; i++) {
+      res = res * b % m;
+      if (resSet.count(res)) {
+        auto width = i - resIndex[res], steps = p - i;
+        res = indexRes[resIndex[res] + (steps % width)];
+        break;
+      } else {
+        resSet.insert(res), indexRes[i] = res, resIndex[res] = i;
+      }
+    }
+
+    cout << res << endl;
+  }
+}
