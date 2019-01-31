5237a84a591e93d369474ab0b69c84ca4fb57a0d
David Guan <davidguandev@gmail.com>
2019-01-31 21:00:39 +1100

    LC 647

---
+++ leetcode/647.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef double f64;
+typedef long long i64;
+typedef int i32;
+typedef pair<i32, i32> pi32;
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef vector<i32> vi32;
+typedef deque<i32> di32;
+
+#define all(c) (c).begin(), (c).end()
+#define REP(i, a, b) for (auto(i) = (a); i < (b); (i++))
+#define REPA(i, a, b, acc) for (auto i = a; i < b; i += acc)
+#define PB push_back
+#define PF push_front
+#define TR(c, it) for (auto(it) = (c).begin(); (it) != (c).end(); (it)++)
+
+class Solution {
+ public:
+  i32 countSubstrings(string s) {
+    vector<vector<bool> > dp;
+
+    REP(i, 0, s.size()) {
+      dp.push_back(vector<bool>(s.size() - i, (i == 0 ? true : false)));
+    }
+
+    REP(i, 1, s.size()) {
+      REP(j, 0, s.size() - i) {
+        if (s[j] != s[j + i]) continue;
+        dp[i][j] = i == 1 ? true : dp [i - 2][j + 1];
+      }
+    }
+
+    i32 sum = 0;
+    TR(dp, it)  TR(*it, d) sum += (*d) ? 1 : 0;
+
+    return sum;
+  }
+};
+
+i32 main() {
+  ios::sync_with_stdio(false);  // Makes IO faster, remove this line if C style scanf/printf needed.
+}
--- time-notes.md
+++ time-notes.md
@@ -1,5 +1,7 @@
 | Problem | Submit Count | Reading & Thinking(mins) | Coding(mins) | Debugging(mins) | Tags |
 | - |:-:|:-:|:-:|:-:|-:|
+|  |  |  |  |  |  |
+| [LC 647](https://leetcode.com/problems/palindromic-substrings/) Palindromic Substrings | 1 | 9 | 16 | 0 | dp & string |
 | [CF 455A](https://codeforces.com/problemset/problem/455/A) Boredom | 3 | 13 | 7 | 3 | dp |
 | [LC 406](https://leetcode.com/problems/queue-reconstruction-by-height/) Queue Reconstruction by Height | 1 | 13 | 10 | 0 | greedy |
 | [CF 415/B](https://codeforces.com/contest/415/problem/B) Mashmokh and Tokens | 1 | 13 | 4 | 0 | greedy |
