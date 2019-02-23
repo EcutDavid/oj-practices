1bf8fc0b7c850bbfbc10a0241fa32b1189147fd7
David Guan <davidguan@canva.com>
2019-02-23 13:17:57 +1100

    temp

---
+++ syd-comp-meetup/password.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef double f64;
+typedef long long i64;
+typedef int i32;
+typedef pair<i32, i32> pi32;
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef vector<i32> vi32;
+typedef deque<i32> di32;
+
+#define all(c) (c).begin(), (c).end()
+#define REP(i, a, b) for (auto i = a; i < b; i++)
+#define REPA(i, a, b, acc) for (auto i = a; i < b; i += acc)
+#define PB push_back
+#define PF push_front
+#define TR(c, it) for (auto (it) = (c).begin(); (it) != (c).end(); (it)++)
+
+const i32 passLen = 5;
+
+string numToStr(i32 num) {
+  string base = to_string(num);
+  return string(passLen - base.size(), '0');
+}
+
+bool validate(string pass, vector<string>& rules) {
+  REP(i, 0, rules.size()) {
+    // Rule's length is three
+
+  }
+  return true;
+}
+
+i32 main()
+{
+  ios::sync_with_stdio(false); // Makes IO faster, remove this line if C style scanf/printf needed.
+
+  // Length of password is five, so 1e6 yields all the comb.
+  REP(i, 0, i32(1e6)) {
+    stirng pass = numToStr(i);
+    if (validate(pass)) {
+      cout << pass << "\n";
+    }
+  }
+  cout << numToStr(0);
+}
