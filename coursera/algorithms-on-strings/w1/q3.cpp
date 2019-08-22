490b281da8fd570433223893ad6e8804709b1d3f
David Guan <davidguan@canva.com>
2019-08-22 19:50:55 +1000

    practice

---
+++ coursera/algorithms-on-strings/w1/q3.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+struct TrieNode {
+  char c;
+  bool isEnd;
+  unordered_map<char, TrieNode *> children;
+};
+
+int main() {
+  string text;
+  int n;
+  cin >> text >> n;
+
+  auto rootNode = new TrieNode{};
+
+  for (int i = 0; i < n; i++) {
+    string word;
+    cin >> word;
+    auto cur = rootNode;
+    for (char c : word) {
+      if (cur->children.count(c)) {
+        cur = cur->children[c];
+      } else {
+        auto newNode = new TrieNode{c};
+        cur->children[c] = newNode;
+        cur = newNode;
+      }
+    }
+    cur->isEnd = true;
+  }
+
+  vector<int> indexes;
+  for (int i = 0; i < text.size(); i++) {
+    auto triePointer = rootNode;
+    auto textPointer = i;
+
+    while(!triePointer->isEnd) {
+      char c = text[textPointer];
+      if (triePointer->children.count(c)) {
+        textPointer++;
+        triePointer = triePointer->children[c];
+      } else {
+        break;
+      }
+    }
+
+    if (triePointer->isEnd) {
+      indexes.push_back(i);
+    }
+  }
+
+  for (int i = 0; i < indexes.size(); i++) {
+    cout << (i > 0 ? " " : "") << indexes[i];
+  }
+}
