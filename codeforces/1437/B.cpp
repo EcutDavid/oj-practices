831502fe83f7611d2031365663e8409919e2b0b0
David Guan <davidguan@canva.com>
2020-11-13 21:53:37 +1100

    Back on practice

---
+++ codeforces/1437/B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef double f64;
+typedef long long i64;
+typedef int i32;
+typedef pair<i32, i32> pi32;
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef vector<i32> vi32;
+typedef deque<i32> di32;
+
+#define all(c) (c).begin(), (c).end()
+#define REP(i, a, b) for (auto i = a; i < b; i++)
+#define REPA(i, a, b, acc) for (auto i = a; i < b; i += acc)
+#define PB push_back
+#define PF push_front
+#define TR(c, it) for (auto (it) = (c).begin(); (it) != (c).end(); (it)++)
+
+i32 main() {
+  ios::sync_with_stdio(false); // Makes IO faster, remove this line if C style scanf/printf needed.
+
+  // Each reverse can resolves two points (or just a single point) of "failures", therefore,
+  // we can resolve the issues by finding out how many failing points are there.
+  i32 t;
+  cin >> t;
+
+  REP(i, 0, t) {
+    i32 n;
+    string str;
+    cin >> n >> str;
+
+    i32 failCount = 0;
+    REP(j, 0, n - 1) {
+      failCount += str[j] == str[j + 1];
+    }
+    cout << failCount / 2 + failCount % 2 << "\n";
+  }
+}
